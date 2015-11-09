2792626456403809ddabf3e8c34fed86ee86f2b5
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-11-09 16:54:22 +0100

    Refactor ConsensusImpl to be more readable

--- src/main/java/itest/consensus/impl/ConsensusImpl.java
+++ src/main/java/itest/consensus/impl/ConsensusImpl.java
@@ -6,20 +6,48 @@ import itest.consensus.Voter;
 import java.util.ArrayList;
 
 public class ConsensusImpl implements Consensus {
+    private ArrayList<Voter> hasVoted = new ArrayList<Voter>();
+
     @Override
     public boolean voting(ArrayList<Voter> voters) {
-        if(null == voters || voters.isEmpty())
+        if(!votersAreValid(voters))
             throw new RuntimeException("Cannot complete the voting because there are no voters.");
 
         boolean result = true;
 
-        for(final Voter v : voters) {
-            if(null == v)
-                continue;
+        for(final Voter voter : voters) {
+            // Cast the vote is the voter is valid and hasn't voted before.
+            if(isValid(voter) && !hasVoted(voter)) {
+                final boolean voteResult = voter.vote();
 
-            if(false == v.vote())
-                result = false;
+                hasVoted.add(voter);
+
+                if(false == voteResult) {
+                    result = false;
+                }
+            }
         }
         return result;
+    }
+
+    /**
+     * Check whether the voter is able to vote.
+     */
+    private boolean isValid(final Voter voter) {
+        return null != voter;
+    }
+
+    /**
+     * Check whether the voters list is usable.
+     */
+    private boolean votersAreValid(final ArrayList<Voter> voters) {
+        return null != voters && !voters.isEmpty();
+    }
+
+    /**
+     * Check whether a voter has voted before.
+     */
+    private boolean hasVoted(final Voter voter) {
+        return hasVoted.contains(voter);
     }
 }
